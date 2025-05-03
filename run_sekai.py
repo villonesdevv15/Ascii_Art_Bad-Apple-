455999f83b80e21fe2e110d851a020fe51e77095
_Bunccep <lorenzchristianvillones@gmail.com>
2025-05-03 14:08:05 +0800

    Update run_sekai.py

--- run_sekai.py
+++ run_sekai.py
@@ -1,12 +1,13 @@
-# cmd running test
+# Run a program through cmd to prevent frame delay
 # sekai version
 import os
 import time
 import pyautogui 
 
 def run_sekai():
-    os.system("start cmd /k cd C:\\Users\\villo\\OneDrive\\Desktop\\ASCII Art Program") #C:\\User\Name\\Location\\Document\Folder
     time.sleep(0.5)
+    os.system("start cmd /k cd C:\\Users\\villo\\OneDrive\\Desktop\\ASCII Art Program") #C:\\User\Name\\Location\\Document\Folder
+    time.sleep(1)
     pyautogui.write("python SEKAI_VER_Bad_Apple!!.py", interval=0.1) # run better without after frames
     pyautogui.press("enter") # executing
     time.sleep(0.5)
@@ -14,5 +15,3 @@ def run_sekai():
 
 if __name__ == "__main__":
     run_sekai()
-
-    
