da49c4b0ac9723912665a476ffb995ae08aac097
_Bunccep <lorenzchristianvillones@gmail.com>
2025-05-03 14:07:47 +0800

    Update run_apple.py

--- run_apple.py
+++ run_apple.py
@@ -5,8 +5,9 @@ import time
 import pyautogui  
 
 def run_bad_apple():
-    os.system("start cmd /k cd C:\\Users\\villo\\OneDrive\\Desktop\\ASCII Art Program")  #C:\\User\Name\\Location\\Document\Folder
     time.sleep(0.5)
+    os.system("start cmd /k cd C:\\Users\\villo\\OneDrive\\Desktop\\ASCII Art Program")  #C:\\User\Name\\Location\\Document\Folder
+    time.sleep(1)
     pyautogui.write("python Bad_Apple!!_ASCII.py", interval=0.1) # run better without after frames
     pyautogui.press("enter") # executing
     time.sleep(0.5)
@@ -14,5 +15,3 @@ def run_bad_apple():
 
 if __name__ == "__main__":
     run_bad_apple()
-
-    
